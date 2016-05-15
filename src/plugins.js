cc9dcfd325ede5198f739bc61771d72b9dd05691
goodmind <andwebar@gmail.com>
2016-05-15 22:07:04 +0500

    style(plugins)

--- src/plugins.js
+++ src/plugins.js
@@ -4,7 +4,10 @@ import { getEntityFirstValue } from './telegram-driver/index'
 export let commandName = (update) => getEntityFirstValue('bot_command')(update)
 
 export let findCommandIn = curryN(2, (commands, path) => {
-  let match = find(([r, _]) => path.match(r), commands) || takeLast(1, commands)[0]
+  let match = find(
+      ([r, _]) => path.match(r),
+      commands
+    ) || takeLast(1, commands)[0]
 
   return [match[0], match[1]]
 })
