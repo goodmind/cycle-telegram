9757f9a9a4746e8029f45e77553b79358766c22b
goodmind <andwebar@gmail.com>
2016-05-15 22:06:33 +0500

    refactor(api-request)

--- src/telegram-driver/api-request.js
+++ src/telegram-driver/api-request.js
@@ -6,7 +6,13 @@ let prepareOptions = (options, query) => Object.assign(
   options, options.httpMethod === 'POST' ? { send: query } : { query }
 )
 
-export function makeAPIRequest ({token, method, query, httpMethod = 'POST', httpDriver = makeHTTPDriver()}) {
+export function makeAPIRequest ({
+  token,
+  method,
+  query,
+  httpMethod = 'POST',
+  httpDriver = makeHTTPDriver()
+}) {
   let endpoint = `https://api.telegram.org/bot${token}`
   let url = `${endpoint}/${method}`
   let uuid = v4()
@@ -21,8 +27,10 @@ export function makeAPIRequest ({token, method, query, httpMethod = 'POST', http
   return httpDriver(request)
     .filter(res => res.request.uuid === uuid)
     .switch()
-    .catch(e => Rx.Observable.throw(new Error(e)))
+    .catch(e => Rx.Observable.throw(e instanceof Error ? e : new Error(e)))
     .map(res => res.body)
-    .map(body => body.ok ? Rx.Observable.just(body.result) : Rx.Observable.throw(new Error(body)))
+    .map(
+      body => body.ok ? Rx.Observable.just(body.result)
+        : Rx.Observable.throw(new Error(body)))
     .switch()
 }
