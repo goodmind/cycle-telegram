e95f4450cf5a9701deb4870737ceaaec4bff9fbb
goodmind <andwebar@gmail.com>
2016-05-17 23:14:20 +0500

    refactor(test)

--- test/index.js
+++ test/index.js
@@ -1,4 +1,10 @@
+import { run } from '@cycle/core'
+import { makeTelegramDriver } from '../lib/index'
+import { Observable as $ } from 'rx'
+
 import test from 'tape'
+
+let basicDriver = makeTelegramDriver('')
 
 test('telegram driver', (t) => {
   t.end()
