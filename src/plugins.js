b455c00477768c8f3d348c1973664a430d2a381d
goodmind <andwebar@gmail.com>
2016-05-30 18:09:47 +0500

    fix(plugins): correct behaviour for `UpdateMessage`

--- src/plugins.js
+++ src/plugins.js
@@ -3,9 +3,14 @@ import {
   UpdateInlineQuery,
   getEntityFirst
 } from './telegram-driver'
-import { find, curryN, prop } from 'ramda'
+import { find, curryN, prop, compose, isNil, not, merge } from 'ramda'
 
+import isolate from '@cycle/isolate'
 import t from 'tcomb'
+
+const UpdateMessageCommand = t.refinement(UpdateMessage,
+  compose(not, isNil, getEntityFirst('bot_command')),
+  'UpdateMessageCommand')
 
 let matchPluginPath = (plugins, str) => find(
   ({path}) => str.match(path),
@@ -22,22 +27,24 @@ let makeResolverWith = curryN(3, (plugins, queryWith, u) => {
   return { plugin, props }
 })
 
-export let matchPlugin = function (plugins) {
+export let matchPlugin = function (plugins, sources) {
   let resolve = makeResolverWith(plugins)
   let type = (u) => t.match(u,
-    UpdateMessage, resolve(
+    UpdateMessageCommand, resolve(
       u => u.message.text.substr(getEntityFirst('bot_command', u).offset)
     ),
     UpdateInlineQuery, resolve(
       u => u.inline_query.query
-    )
+    ),
+    UpdateMessage, resolve(u => u.message.text)
   )
 
   return this.map(u => {
     let match = type(u)
     let component = {}
+    let args = merge({props: match.props}, sources)
     if (match.plugin.type.is(u)) {
-      component = match.plugin.component({props: match.props}, u) || {}
+      component = isolate(match.plugin.component)(args, u) || {}
     }
     return component
   }).filter(prop('bot'))
