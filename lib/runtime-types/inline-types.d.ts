5abe29773c805f11c484642c9a211cca38c734ee
goodmind <andwebar@gmail.com>
2016-11-05 17:59:05 +0500

    update lib

--- lib/runtime-types/inline-types.d.ts
+++ lib/runtime-types/inline-types.d.ts
@@ -6,21 +6,20 @@ export interface TcombInputMessageContent {
     parse_mode?: string;
     disable_web_page_preview?: boolean;
 }
-export declare const InlineQueryResult: t.Struct<TcombInlineQueryResult>;
-export interface TcombInlineQueryResult {
+export interface BaseInlineQueryResult {
     type: string;
     id: string;
     reply_markup?: k.TcombInlineKeyboardMarkup;
     input_message_content?: TcombInputMessageContent;
 }
 export declare const InlineQueryResultCachedAudio: t.Struct<TcombInlineQueryResultCachedAudio>;
-export interface TcombInlineQueryResultCachedAudio extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultCachedAudio extends BaseInlineQueryResult {
     type: 'audio';
     audio_file_id: string;
     caption?: string;
 }
 export declare const InlineQueryResultCachedDocument: t.Struct<TcombInlineQueryResultCachedDocument>;
-export interface TcombInlineQueryResultCachedDocument extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultCachedDocument extends BaseInlineQueryResult {
     type: 'document';
     title: string;
     document_file_id: string;
@@ -28,25 +27,25 @@ export interface TcombInlineQueryResultCachedDocument extends TcombInlineQueryRe
     caption?: string;
 }
 export declare const InlineQueryResultCachedGif: t.Struct<TcombInlineQueryResultCachedGif>;
-export interface TcombInlineQueryResultCachedGif extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultCachedGif extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultCachedMpeg4Gif: t.Struct<TcombInlineQueryResultCachedMpeg4Gif>;
-export interface TcombInlineQueryResultCachedMpeg4Gif extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultCachedMpeg4Gif extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultCachedPhoto: t.Struct<TcombInlineQueryResultCachedPhoto>;
-export interface TcombInlineQueryResultCachedPhoto extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultCachedPhoto extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultCachedSticker: t.Struct<TcombInlineQueryResultCachedSticker>;
-export interface TcombInlineQueryResultCachedSticker extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultCachedSticker extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultCachedVideo: t.Struct<TcombInlineQueryResultCachedVideo>;
-export interface TcombInlineQueryResultCachedVideo extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultCachedVideo extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultCachedVoice: t.Struct<TcombInlineQueryResultCachedVoice>;
-export interface TcombInlineQueryResultCachedVoice extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultCachedVoice extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultArticle: t.Struct<TcombInlineQueryResultArticle>;
-export interface TcombInlineQueryResultArticle extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultArticle extends BaseInlineQueryResult {
     type: 'article';
     title: string;
     input_message_content: TcombInputMessageContent;
@@ -59,35 +58,39 @@ export interface TcombInlineQueryResultArticle extends TcombInlineQueryResult {
     thumb_height?: number;
 }
 export declare const InlineQueryResultAudio: t.Struct<TcombInlineQueryResultAudio>;
-export interface TcombInlineQueryResultAudio extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultAudio extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultContact: t.Struct<TcombInlineQueryResultContact>;
-export interface TcombInlineQueryResultContact extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultContact extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultGame: t.Struct<TcombInlineQueryResultGame>;
-export interface TcombInlineQueryResultGame extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultGame extends BaseInlineQueryResult {
+    type: 'game';
+    game_short_name: string;
 }
 export declare const InlineQueryResultDocument: t.Struct<TcombInlineQueryResultDocument>;
-export interface TcombInlineQueryResultDocument extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultDocument extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultGif: t.Struct<TcombInlineQueryResultGif>;
-export interface TcombInlineQueryResultGif extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultGif extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultLocation: t.Struct<TcombInlineQueryResultLocation>;
-export interface TcombInlineQueryResultLocation extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultLocation extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultMpeg4Gif: t.Struct<TcombInlineQueryResultMpeg4Gif>;
-export interface TcombInlineQueryResultMpeg4Gif extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultMpeg4Gif extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultPhoto: t.Struct<TcombInlineQueryResultPhoto>;
-export interface TcombInlineQueryResultPhoto extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultPhoto extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultVenue: t.Struct<TcombInlineQueryResultVenue>;
-export interface TcombInlineQueryResultVenue extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultVenue extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultVideo: t.Struct<TcombInlineQueryResultVideo>;
-export interface TcombInlineQueryResultVideo extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultVideo extends BaseInlineQueryResult {
 }
 export declare const InlineQueryResultVoice: t.Struct<TcombInlineQueryResultVoice>;
-export interface TcombInlineQueryResultVoice extends TcombInlineQueryResult {
+export interface TcombInlineQueryResultVoice extends BaseInlineQueryResult {
 }
+export declare const InlineQueryResult: t.Union<TcombInlineQueryResult>;
+export declare type TcombInlineQueryResult = TcombInlineQueryResultCachedAudio | TcombInlineQueryResultCachedDocument | TcombInlineQueryResultCachedGif | TcombInlineQueryResultCachedMpeg4Gif | TcombInlineQueryResultCachedPhoto | TcombInlineQueryResultCachedSticker | TcombInlineQueryResultCachedVideo | TcombInlineQueryResultCachedVoice | TcombInlineQueryResultArticle | TcombInlineQueryResultAudio | TcombInlineQueryResultContact | TcombInlineQueryResultGame | TcombInlineQueryResultDocument | TcombInlineQueryResultGif | TcombInlineQueryResultLocation | TcombInlineQueryResultMpeg4Gif | TcombInlineQueryResultPhoto | TcombInlineQueryResultVenue | TcombInlineQueryResultVideo | TcombInlineQueryResultVoice;
--- lib/runtime-types/keyboard-types.d.ts
+++ lib/runtime-types/keyboard-types.d.ts
@@ -1,7 +1,38 @@
 import * as t from 'tcomb';
-export declare const KeyboardButton: t.Struct<{}>;
-export declare const ReplyKeyboardMarkup: t.Struct<{}>;
-export declare const ReplyKeyboardHide: t.Struct<{}>;
-export declare const InlineKeyboardButton: t.Struct<{}>;
-export declare const InlineKeyboardMarkup: t.Struct<{}>;
-export declare const ForceReply: t.Struct<{}>;
+import { TcombCallbackGame } from './types';
+export declare const KeyboardButton: t.Struct<TcombKeyboardButton>;
+export interface TcombKeyboardButton {
+    text: string;
+    request_contact?: boolean;
+    request_location?: boolean;
+}
+export declare const ReplyKeyboardMarkup: t.Struct<TcombReplyKeyboardMarkup>;
+export interface TcombReplyKeyboardMarkup {
+    keyboard: TcombKeyboardButton[][];
+    resize_keyboard?: boolean;
+    one_time_keyboard?: boolean;
+    selective?: boolean;
+}
+export declare const ReplyKeyboardHide: t.Struct<TcombReplyKeyboardHide>;
+export interface TcombReplyKeyboardHide {
+    hide_keyboard: boolean;
+    selective?: boolean;
+}
+export declare const InlineKeyboardButton: t.Struct<TcombInlineKeyboardButton>;
+export interface TcombInlineKeyboardButton {
+    text: string;
+    url?: string;
+    callback_data?: string;
+    callback_game?: TcombCallbackGame;
+    switch_inline_query?: string;
+    switch_inline_query_current_chat?: string;
+}
+export declare const InlineKeyboardMarkup: t.Struct<TcombInlineKeyboardMarkup>;
+export interface TcombInlineKeyboardMarkup {
+    inline_keyboard: TcombInlineKeyboardButton[][];
+}
+export declare const ForceReply: t.Struct<TcombForceReply>;
+export interface TcombForceReply {
+    force_reply: boolean;
+    selective?: boolean;
+}
