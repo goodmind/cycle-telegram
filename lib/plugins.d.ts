922b429bb05f23cdaae72f142fce4dc3d6322f86
goodmind <andwebar@gmail.com>
2016-11-27 20:37:06 +0500

    update lib

--- lib/plugins.d.ts
+++ lib/plugins.d.ts
@@ -9,12 +9,27 @@ export declare type ComponentSinks = {
     [driverName: string]: GenericStream<any>;
 } | void;
 export declare type Component = (sources: ComponentSources, update: TcombUpdate) => ComponentSinks;
-export interface Plugin {
+export declare type Plugin = {
     type: t.Type<any>;
-    name: string;
     pattern?: RegExp;
+    name?: string;
     component: Component;
-}
+} | {
+    type?: t.Type<any>;
+    pattern: RegExp;
+    name?: string;
+    component: Component;
+} | {
+    type: t.Type<any>;
+    pattern: RegExp;
+    name?: string;
+    component: Component;
+} | {
+    type?: t.Type<any>;
+    pattern?: RegExp;
+    name?: string;
+    component: Component;
+};
 export interface PluginsExecution {
     matchWith(this: GenericStream<TcombUpdate>, plugins: Plugin[], sources: ComponentSources, {dupe}?: {
         dupe?: boolean;
