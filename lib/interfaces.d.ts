b3fa2b225806a26e77168fb57c7306ca6b570d35
goodmind <andwebar@gmail.com>
2016-12-04 15:24:35 +0500

    update lib

--- lib/interfaces.d.ts
+++ lib/interfaces.d.ts
@@ -3,7 +3,7 @@ import { TcombWebhookResponse, TcombRequest, TcombUpdate, TcombUpdatesState } fr
 export declare type Token = string;
 export declare type GenericStream<T> = any;
 export declare type DriverSink = TcombRequest | TcombWebhookResponse;
-export declare type EventNames = 'message' | 'channel_post' | 'inline_query' | 'chosen_inline_result' | 'callback_query';
+export declare type EventNames = 'message' | 'channel_post' | 'edited_channel_post' | 'inline_query' | 'chosen_inline_result' | 'callback_query';
 export interface DriverOptions {
     webhook?: boolean;
     startDate?: number;
@@ -12,6 +12,7 @@ export interface DriverOptions {
 export interface DriverSources {
     message: Observable<TcombUpdate>;
     channelPost: Observable<TcombUpdate>;
+    editedChannelPost: Observable<TcombUpdate>;
     inlineQuery: Observable<TcombUpdate>;
     chosenInlineResult: Observable<TcombUpdate>;
     callbackQuery: Observable<TcombUpdate>;
